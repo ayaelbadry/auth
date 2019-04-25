a08186c3514e76a480afbd47b8c43865e0d20eff
Aya <aya@192.168.184.130>
2019-04-25 11:05:57 -0700

    make email and password design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Header} from './components/common';
+import LoginForm from './components/LoginForm'
 import firebase from 'firebase';
 class App extends  Component {
     componentWillMount(){
@@ -14,11 +15,11 @@ class App extends  Component {
         })
     }
     render(){
-        
+
         return(
             <View>
                 <Header headerText='Authentication' />
-                <Text> App Here </Text>
+                <LoginForm />
             </View>
         );
     }
