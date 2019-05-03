8aa3f38cc63976497a998e6463b18f8227abe4ee
IntelSoftSystem <intelsoft14@gmail.com>
2019-05-03 10:57:10 -0700

    finish login & logout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React , {Component} from 'react';
 import {View, Text} from 'react-native';
-import {Header} from './components/common';
+import {Header, Button, Spinner, CardSection, Card} from './components/common';
 import LoginForm from './components/LoginForm'
 import firebase from 'firebase';
 class App extends  Component {
+  state = {
+    loggedIn: null
+  }
     componentWillMount(){
         firebase.initializeApp({
             apiKey: "AIzaSyBITvWYKXtMq7zqYQ5QR1MgFv2CR_FjdVg",
@@ -13,16 +16,49 @@ class App extends  Component {
             storageBucket: "auth-67c40.appspot.com",
             messagingSenderId: "483139747544"
         })
+        firebase.auth().onAuthStateChanged((user) => {
+          if(user){
+            this.setState({loggedIn: true})
+          }else{
+            this.setState({loggedIn: false})
+          }
+
+        })
+    }
+    renderContent(){
+      switch (this.state.loggedIn) {
+        case true:
+            return (
+              <CardSection>
+                  <Button onPress={ () => firebase.auth().signOut()}> Log Out </Button>
+              </CardSection>
+          );
+
+        case false:
+            return <LoginForm />
+        default:
+             return <Spinner size="large" />
+
+
+      }
     }
     render(){
 
         return(
-            <View>
+            <View >
                 <Header headerText='Authentication' />
-                <LoginForm />
+
+                   {this.renderContent()}
+
             </View>
         );
     }
 
+}
+const styles = {
+  logoutStyle: {
+    paddingTop: 20,
+    alignSelf: 'center'
+  }
 }
 export default App
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,50 @@
 import React, {Component} from 'react';
-import { TextInput } from 'react-native';
-import {Button, Card, CardSection, Input } from './common'
-
+import { TextInput, Text } from 'react-native';
+import {Button, Card, CardSection, Input, Spinner } from './common'
+import firebase from 'firebase'
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: '',
+    loading: false
   }
+  onPressButton(){
+    const {email, password} = this.state
+    this.setState({error: '', loading: true})
+    // must found catch statement to catch any error that occered
+    firebase.auth().signInWithEmailAndPassword(email, password)
+     .then(this.onLoginSuccess.bind(this))
+     .catch(() => {
+       firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(this.onLoginSuccess.bind(this))
+        .catch(this.onLoginFail.bind(this))
+     })
+  }
+  //helper method to make condition to show the spinner or the button
+  renderButton(){
+    if (this.state.loading){
+      return <Spinner size="small"/>;
+    }
+    return(
+      <Button onPress={this.onPressButton.bind(this)} >Log In </Button>
+    )
+  }
+  // onLoginSuccess to invoke through then method to clear the spinner and any error message that appear on the screen
+//while login success
+  onLoginSuccess(){
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    })
+  }
+  //to clear the spinner and show error while login failed
+  onLoginFail(){
+    this.setState({error: 'Authentication Failed', loading: false})
+  }
+//  const { errorTextStyle } = styles
   render(){
     return(
       <Card>
@@ -31,12 +69,20 @@ class LoginForm extends Component {
 
 
          </CardSection>
+         <Text style={styles.errorTextStyle}>{this.state.error}</Text>
          <CardSection>
-             <Button>Log In </Button>
+             {this.renderButton()}
          </CardSection>
 
       </Card>
     );
+  }
+}
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    color: 'red',
+    alignSelf: 'center'
   }
 }
 export default LoginForm;
